9e25ee6c71d057c3cd6f15495e3aaa47dcb28a67
Mikel Martín Corrales <mikel.martin.corrales@gmail.com>
2025-07-05 19:49:35 +0200

    Improve generation sctipts

--- generatepdf.bat
+++ generatepdf.bat
@@ -3,19 +3,28 @@ cd /d "%~dp0"
 
 where pandoc >nul 2>&1
 if errorlevel 1 (
-    echo [ERROR] Pandoc is not installed o it is not in the PATH.
+    echo [ERROR] Pandoc is not installed or it is not in the PATH.
     echo Install Pandoc from https://pandoc.org/installing.html
     pause
     exit /b 1
 )
 
-pandoc main.md --no-highlight --template=tex/template.tex --pdf-engine=xelatex --filter pandoc-latex-environment --lua-filter=filters/filters.lua -o output/main.pdf
-
-if errorlevel 1 (
-    echo [ERROR] There was a problem generating the PDF.
-    pause
-    exit /b 1
+echo|set /p="Generating pdf"
+start /b "" cmd /c "pandoc main.md --no-highlight --template=tex/template.tex --pdf-engine=xelatex --filter pandoc-latex-environment --lua-filter=filters/filters.lua -o output\main.pdf 2>nul"
+:waitloop
+timeout /t 1 >nul
+tasklist | find /i "pandoc.exe" >nul
+if %errorlevel%==0 (
+    set /p="." <nul
+    goto waitloop
 )
 
-echo PDF successfully generated in output\main.pdf
+if exist output\main.pdf (
+    echo.
+    echo PDF successfully generated in output\main.pdf
+) else (
+    echo.
+    echo [ERROR] There was a problem generating the PDF.
+)
+
 pause
--- generatepdf.sh
+++ generatepdf.sh
@@ -2,16 +2,28 @@
 cd "$(dirname "$0")"
 
 if ! command -v pandoc >/dev/null 2>&1; then
-    echo [ERROR] Pandoc is not installed o it is not in the PATH.
-    echo Install Pandoc from https://pandoc.org/installing.html
+    echo "[ERROR] Pandoc is not installed or it is not in the PATH."
+    echo "Install Pandoc from https://pandoc.org/installing.html"
     exit 1
 fi
 
-pandoc main.md --no-highlight --template=tex/template.tex --pdf-engine=xelatex --filter pandoc-latex-environment --lua-filter=filters/filters.lua -o output/main.pdf
+echo -n "Generating pdf"
+pandoc main.md --no-highlight --template=tex/template.tex --pdf-engine=xelatex --filter pandoc-latex-environment --lua-filter=filters/filters.lua -o output/main.pdf 2>/dev/null &
+pid=$!
 
-if [ $? -ne 0 ]; then
-    echo [ERROR] There was a problem generating the PDF.
+while kill -0 $pid 2>/dev/null; do
+    echo -n "."
+    sleep 0.5
+done
+
+wait $pid
+status=$?
+
+if [ $status -ne 0 ]; then
+    echo
+    echo "[ERROR] There was a problem generating the PDF."
     exit 1
 fi
 
-echo PDF successfully generated in output\\main.pdf
+echo
+echo "PDF successfully generated in output/main.pdf"
